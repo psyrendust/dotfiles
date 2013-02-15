1b28c8f6754b4562f675366936cf7e897c8a9d05
Larry Gordon <larry@johnmcneilstudio.com>
2013-02-14 16:05:37 -0800

    Updates.

--- install-deps.sh
+++ install-deps.sh
@@ -5,20 +5,39 @@
 # the `push` command which copies the github compare URL to my clipboard is heaven
 sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
+# I'm assuming that you have xCode 4.5.x+ installed. If so rvm won't work
+# because of:
+#   The provided compiler '/usr/bin/gcc' is LLVM based, it is not yet fully
+#   supported by ruby and gems, please read `rvm requirements`.
+# Fix is here:
+#   http://caiustheory.com/install-gcc-421-apple-build-56663-with-xcode-42
+# Instructions:
+# 1. Grab and unpack the tarball
+#   mkdir ~/tmp && cd ~/tmp;
+#   curl -O http://opensource.apple.com/tarballs/gcc/gcc-5666.3.tar.gz;
+#   tar zxf gcc-5666.3.tar.gz && cd gcc-5666.3;
+# 2. Setup some stuff it requires
+#   mkdir -p build/obj build/dst build/sym
+# And then build it. You should go make a cup of tea or five whilst this runs.
+#   gnumake install RC_OS=macos RC_ARCHS='i386 x86_64' TARGETS='i386 x86_64' SRCROOT=`pwd` OBJROOT=`pwd`/build/obj DSTROOT=`pwd`/build/dst SYMROOT=`pwd`/build/sym;
+# 3. And finally install it
+#   sudo ditto build/dst /
+#   rm -rf ~/tmp
+
 # https://rvm.io
 # rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
-rvm install 1.8.7
-rvm install 1.9.2
-rvm --default 1.9.2
-ruby -v
-which ruby
-gem install rails
+curl -L https://get.rvm.io | bash -s stable --ruby;
+rvm install 1.8.7;
+rvm install 1.9.2;
+rvm --default 1.9.2;
+ruby -v;
+which ruby;
+gem install rails;
 
 # Install Compass
 # http://compass-style.org/install/
-gem update --system
-gem install compass
+gem update --system;
+gem install compass;
 
 # homebrew!
 # you need the code CLI tools YOU FOOL.
