4c27f5ce964f66fe6648f3d024211cc97f68e7e7
Larry Gordon <lgordon@psyrendust.com>
2013-07-17 13:55:42 -0700

    Updates

--- install-deps.sh
+++ install-deps.sh
@@ -7,11 +7,6 @@ sudo groupadd -G $USER wheel
 # Change ownership of /usr/local
 sudo chown -R $USER /usr/local
 
-
-# https://github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-# sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
 # I'm assuming that you have xCode 4.5.x+ installed. If so rvm won't work
 # because of:
 #   The provided compiler '/usr/bin/gcc' is LLVM based, it is not yet fully
@@ -32,15 +27,17 @@ sudo chown -R $USER /usr/local
 #   rm -rf ~/tmp
 
 # install some homebrew stuff
-. .brew
+# . .brew
 
 # https://rvm.io
 # rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby;
+# curl -L https://get.rvm.io | bash -s stable --ruby;
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
-rvm install 1.8.7;
+# rvm install 1.8.7;
 rvm install 1.9.3;
 rvm --default 1.9.3;
+rvm use 1.9.3;
 ruby -v;
 which ruby;
 gem install rails;
@@ -52,60 +49,28 @@ gem install compass;
 
 # install nvm "Node Version Manager"
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+[[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.nvm/nvm.sh"
 
 # install the latest version of node
-nvm install v0.8.23
-nvm install v0.9.12
-nvm install v0.10.5
+nvm install v0.8
+nvm install v0.9
+nvm install v0.10
 nvm alias default 0.8
+nvm use 0.8
 
 # install npm
 curl https://npmjs.org/install.sh | sh
 
-# install Yeoman, Bower, and Grunt Init
-npm install -g yo bower grunt-init grunt-cli jshint
+# # install Yeoman, Bower, and Grunt Init
+# npm install -g yo bower grunt-init grunt-cli jshint
 
-# install some templates
-mkdir ~/.grunt-init
-git clone git@github.com:gruntjs/grunt-init-gruntfile.git ~/.grunt-init/gruntfile
-git clone git@github.com:gruntjs/grunt-init-commonjs.git ~/.grunt-init/commonjs
-git clone git@github.com:gruntjs/grunt-init-gruntplugin.git ~/.grunt-init/gruntplugin
-git clone git@github.com:gruntjs/grunt-init-jquery.git ~/.grunt-init/jquery
-git clone git@github.com:gruntjs/grunt-init-node.git ~/.grunt-init/node
-
-# https://github.com/isaacs/nave
-# needs npm, obviously.
-# TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-# npm install -g nave
-
-# https://github.com/rupa/z
-# z, oh how i love you
-# mkdir -p ~/dev/z
-# curl https://raw.github.com/rupa/z/master/z.sh > ~/dev/z/z.sh
-# chmod +x ~/dev/z/z.sh
-# also consider moving over your current .z file if possible. it's painful to rebuild :)
-
-# add this to the bash_profile file if it aint there.
-#   . ~/dev/z/z.sh
-
-
-# cd ~/dev
-# git clone git://github.com/dronir/SpotifyControl.git
-
-
-
-# my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures
-# then start Photo Booth. It'll ask where to put the library.
-# put it in Dropbox/public
-
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
-
+# # install some templates
+# mkdir ~/.grunt-init
+# git clone git@github.com:gruntjs/grunt-init-gruntfile.git ~/.grunt-init/gruntfile
+# git clone git@github.com:gruntjs/grunt-init-commonjs.git ~/.grunt-init/commonjs
+# git clone git@github.com:gruntjs/grunt-init-gruntplugin.git ~/.grunt-init/gruntplugin
+# git clone git@github.com:gruntjs/grunt-init-jquery.git ~/.grunt-init/jquery
+# git clone git@github.com:gruntjs/grunt-init-node.git ~/.grunt-init/node
 
 # for the c alias (syntax highlighted cat)
 sudo easy_install Pygments
-
-
-# chrome canary as default
-# on a mac you can set chrome canary as your default inside of Safari preferences :)
